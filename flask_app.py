e3ddf0a9bce0ae96858963b5c608e8537aad581d
Julio Delgado <juliom6@gmail.com>
2020-02-26 17:04:33 -0300

    flask agora servindo paginas estaticas

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def ola_mundo():
     return '<h1>Primeiro deploy do projeto Flask da disciplina TecWeb 2C - Impacat!</h1>'
+
+# Nossa pagina de de inicio
+@app.route('/inicio')
+def inicio():
+	return render_template('inicio.html')
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
