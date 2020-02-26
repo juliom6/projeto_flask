92796abded91003b79faa1a61363ef21625a867d
Julio Delgado <juliom6@gmail.com>
2020-02-26 16:57:03 -0300

    condigurado servidor de teste

--- flask_app.py
+++ flask_app.py
@@ -4,5 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def ola_mundo():
-    return '<h1>Primeiro deploy do projeto Flask da disciplina TecWeb - Impacat!</h1>'
+    return '<h1>Primeiro deploy do projeto Flask da disciplina TecWeb 2C - Impacat!</h1>'
 
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0')
